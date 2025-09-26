3f0188b906f46e36a9e7baa03193909c96adf8d4
iriacardiel <iriacardielperez@gmail.com>
2025-09-26 12:45:20 +0200

    refactor search script

--- query_friends_graph.py
+++ query_friends_graph.py
@@ -1,76 +1,83 @@
-# Dependencies
-
+"""
+Friends KG - Search & Retrieval Script
+"""
+# === Dependencies
 import os
-from dotenv import load_dotenv  
-import yaml
-from pathlib import Path
-from pprint import pprint
+from dotenv import load_dotenv
 from termcolor import cprint
+from pprint import pprint
+
+
+# === Neo4j / LangChain
 from langchain_neo4j import Neo4jGraph
-import json
 
-from helpers import helper_folium, helper_leaflet, helper_neo4j, helper_ollama
+# === Local helpers
+from helpers import helper_neo4j
 
-# Environment variables
+# -----------------------------------------------------------------------------
+# Config
+# -----------------------------------------------------------------------------
 
 load_dotenv()  # Load local environment variables
 
 URI = "bolt://localhost:" + os.environ.get("URI_PORT")
-NEO4J_USER = os.environ.get("NEO4J_USER")
-NEO4J_PWD = os.environ.get("NEO4J_PASSWORD")
-NEO4J_DB = os.getenv("NEO4J_DATABASE", "neo4j")    # 👈 choose DB here
+NEO4J_USER = os.getenv("NEO4J_USER")
+NEO4J_PWD = os.getenv("NEO4J_PASSWORD")
+NEO4J_DB = os.getenv("NEO4J_DATABASE")
 
-cprint(f"Connecting to Neo4j at {URI} with user {NEO4J_USER} and password {NEO4J_PWD}", "blue")
+# -----------------------------------------------------------------------------
+# Vector Search
+# -----------------------------------------------------------------------------
 
-# Load cypher queries
-
-queries = yaml.safe_load(Path("data/friends/queries_friends.yaml").read_text())
-
-# Neo4j Langchain wrapper instance
-
+# Connect
 kg = Neo4jGraph(url=URI, username=NEO4J_USER, password=NEO4J_PWD, database=NEO4J_DB)
+cprint(f"\nConnected to Neo4j database: {NEO4J_DB}", "green")
 
 # KG RAG Search
+if __name__ == "__main__":
 
-# Query Nodes
-result = helper_neo4j.neo4j_KGRAG_search(runner = kg.query,
-                             element = "node",
-                             query = "Who dances?", 
-                             index = "person_node_idx",
-                             source_property = "text",
-                             main_property = "name",
-                             top_k = 5
-                             )
-pprint(result, width = 200, sort_dicts=False, indent=2)
-file = "data/friends/friends_context_1.txt"
-with open(file, 'w', encoding='utf-8') as f:
-  f.write(result.get("combined_context", ""))
-
-result  = helper_neo4j.neo4j_KGRAG_search(runner = kg.query,
+    # Query Person Nodes
+    result = helper_neo4j.neo4j_KGRAG_search(runner = kg.query,
                                 element = "node",
-                                query = "Which company investigates Cancer?",
-                                index = "company_node_idx",
+                                query = "Who likes horror movies?", 
+                                index = "person_node_idx",
                                 source_property = "text",
                                 main_property = "name",
-                                top_k = 5
-                              )
-pprint(result, width = 200, sort_dicts=False, indent=2)
-file = "data/friends/friends_context_2.txt"
-with open(file, 'w', encoding='utf-8') as f:
-  f.write(result.get("combined_context", ""))
-  
-  
-# Query Relationships
-result  = helper_neo4j.neo4j_KGRAG_search(runner = kg.query,
-                              element = "relationship",
-                              query = "Who is helping Iria at work?",
-                              index = "know_relationship_idx",
-                              source_property = "text",
-                              main_property = "name",
-                              top_k = 5
-                              )
-pprint(result, width = 200, sort_dicts=False)
+                                top_k = 1
+                                )
+    pprint(result, width = 200, sort_dicts=False, indent=2)
 
-file = "data/friends/friends_context_3.txt"
-with open(file, 'w', encoding='utf-8') as f:
-  f.write(result.get("combined_context", ""))
+    file = "data/friends/friends_context_1.txt"
+    with open(file, 'w', encoding='utf-8') as f:
+      f.write(result.get("combined_context", ""))
+
+    # Query Company Nodes
+    result  = helper_neo4j.neo4j_KGRAG_search(runner = kg.query,
+                                    element = "node",
+                                    query = "Which company works for the army?",
+                                    index = "company_node_idx",
+                                    source_property = "text",
+                                    main_property = "name",
+                                    top_k = 1
+                                  )
+    pprint(result, width = 200, sort_dicts=False, indent=2)
+
+    file = "data/friends/friends_context_2.txt"
+    with open(file, 'w', encoding='utf-8') as f:
+      f.write(result.get("combined_context", ""))
+      
+      
+    # Query KNOWS Relationships
+    result  = helper_neo4j.neo4j_KGRAG_search(runner = kg.query,
+                                  element = "relationship",
+                                  query = "Who's Daniel's girlfriend?",
+                                  index = "know_relationship_idx",
+                                  source_property = "text",
+                                  main_property = "name",
+                                  top_k = 1
+                                  )
+    pprint(result, width = 200, sort_dicts=False)
+
+    file = "data/friends/friends_context_3.txt"
+    with open(file, 'w', encoding='utf-8') as f:
+      f.write(result.get("combined_context", ""))
